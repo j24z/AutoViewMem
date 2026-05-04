e3637784224628a99de151450ae421d09811287a
zjcao666 <166584323+zjcao666@users.noreply.github.com>
2026-05-04 22:48:43 +0800

    Remove unused layer memory wrappers

--- autoviewmem/memory/__init__.py
+++ autoviewmem/memory/__init__.py
@@ -1,12 +1,7 @@
-from .core import L0RawMemory, L1EventFrame, L2SchemaMemory, ResolutionPolicy
 from .adaptive import AdaptiveMemoryLayer, SchemaDiscovery
 from .config import ARK_MODEL, ARK_BASE_URL, ARK_API_KEY
 
 __all__ = [
-    'L0RawMemory',
-    'L1EventFrame',
-    'L2SchemaMemory',
-    'ResolutionPolicy',
     'AdaptiveMemoryLayer',
     'SchemaDiscovery',
     'ARK_MODEL',
--- autoviewmem/memory/core/__init__.py
+++ autoviewmem/memory/core/__init__.py
@@ -1,11 +1,1 @@
-from .l0_raw_memory import L0RawMemory
-from .l1_event_frame import L1EventFrame
-from .l2_schema_memory import L2SchemaMemory
-from .resolution_policy import ResolutionPolicy
-
-__all__ = [
-    "L0RawMemory",
-    "L1EventFrame",
-    "L2SchemaMemory",
-    "ResolutionPolicy",
-]
+"""Core memory components."""
